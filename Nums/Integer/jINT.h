f70cfb616590ca66b25448641e900b760ae66151
JAKE DALTON <jd0932925@otc.edu>
2025-03-07 12:18:58 -0600

    Added GMP library

---
+++ Nums/Integer/jINT.h
@@ -0,0 +1,13 @@
+#include<iostream>
+
+class jint
+{
+    public:
+    
+    bool Sign;      //Sign of the number. 0 => neg, 1 => pos//
+
+    unsigned long long Significand;     //Number of digits//
+
+    
+
+};
--- Nums/Integer/main.cpp
+++ Nums/Integer/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-//for debugging integers and idk
+#include <gmp.h>
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    mpz_t a, b, result;
+    mpz_init_set_str(a, "12345678901234567890", 10);
+    mpz_init_set_str(b, "98765432109876543210", 10);
+    mpz_init(result);
+   // mpz_init(sd) = 32965923692370508180386925692364982374;
+    mpz_add(result, a, b);
+
+    std::cout << "Sum: ";
+    mpz_out_str(stdout, 10, result);
+    std::cout << std::endl;
+
+    mpz_clear(a);
+    mpz_clear(b);
+    mpz_clear(result);
+   
+    std::string butt = "ieuhr ";
+    
+    std::cin>> butt;
+
     return 0;
 }
